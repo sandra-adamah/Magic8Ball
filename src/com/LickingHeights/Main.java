e4f44cf4314d5f7c57ed1b81cdc5c69b82db9e4a
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-02-28 12:54:52 -0500

    Put together questions

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,10 +6,37 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //declare
+
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
 
-        
+       int aNumber;
+
+        //initialization phase
+
+        System.out.println("Ask a yes or no/true or false question");
+        keyboard.nextLine();
+
+
+        System.out.println("Choose a number 1-100");
+        aNumber = keyboard.nextInt();
+
+
+            if ((aNumber >= 0) && (aNumber <= 10)) {
+                System.out.println("Of course!");
+            }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
