6c4885b8be8f5f66bcd3e4fe995f4c443277c05f
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-02-28 13:13:22 -0500

    Set up responses for numbers 51-100

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -38,6 +38,21 @@ public class Main {
             }
             else if ((aNumber>= 41) && (aNumber <= 50)) {
                 System.out.println("Yes yes yes yes, couldn't have thought of anything better!");
+            }
+            else if ((aNumber>= 51) && (aNumber <= 60)) {
+                System.out.println("Nooooooooooooooooooo");
+            }
+            else if ((aNumber>= 61) && (aNumber <= 70)) {
+                System.out.println("No, why would you even ask that?");
+            }
+            else if ((aNumber>= 71) && (aNumber <= 80)) {
+                System.out.println("Absolutely.... not.");
+            }
+            else if ((aNumber>= 81) && (aNumber <= 90)) {
+                System.out.println("Nice try but no.");
+            }
+            else if ((aNumber>= 91) && (aNumber <= 100)) {
+                System.out.println("Negative");
 
 
 
